00a3e2049f91b3c83e430b9da3108f23a39783df
UnderhisWing <underhiswings.k@gmail.com>
2018-10-09 22:57:08 +0900

    lecture update

---
+++ be/routes/api/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var createError = require('http-errors');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/hello', function(req, res, next) {
+  res.send({msg:'hello', a:1});
+});
+
+router.get('/user', function(req, res, next) {
+  res.send({msg:'hello', a:1});
+});
+
+router.use('/test', require('./test'));
+
+router.all('*', function(req, res, next) {
+  next(createError(404, '그런 api 없어요'));
+});
+
+module.exports = router;
---
+++ be/routes/api/test/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var createError = require('http-errors');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.send({msg:'hello', a:'all right'});
+});
+/* GET home page. */
+router.get('/hello', function(req, res, next) {
+  res.send({msg:'hello', a:2222});
+});
+
+
+router.all('*', function(req, res, next) {
+  next(createError(404, '그런 api 없어요'));
+});
+
+module.exports = router;
